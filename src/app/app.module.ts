08c2e328c4e9e39313c290f614d53c2c7222fd59
otaviopetry <otaviopetry@gmail.com>
2022-05-30 22:00:19 -0300

    chore: rename routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from 'src/app/pages/home/home.component';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.routes';
 import { AppComponent } from './app.component';
 
 @NgModule({

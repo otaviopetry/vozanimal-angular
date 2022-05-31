c998d9ebcacac1687e61238331dcef3ed89ff671
otaviopetry <otaviopetry@gmail.com>
2022-05-30 22:30:10 -0300

    feat: add eslint and prettier

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
-import { ChangeDetectionStrategy, Component } from "@angular/core";
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 
 @Component({
-  selector: 'va-home',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  templateUrl: 'home.html',
-  styleUrls: ['home.scss'],
+    selector: 'va-home',
+    changeDetection: ChangeDetectionStrategy.OnPush,
+    templateUrl: 'home.html',
+    styleUrls: ['home.scss'],
 })
 export class HomeComponent {
-  //
+    //
 }

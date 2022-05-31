7e52e0124165084563bac516f018730b889b86ea
otaviopetry <otaviopetry@gmail.com>
2022-05-30 22:54:24 -0300

    chore: lint project

--- src/app/pages/adote/adote.component.spec.ts
+++ src/app/pages/adote/adote.component.spec.ts
@@ -1,5 +1,4 @@
 import { AdoteComponent } from 'src/app/pages/adote/adote.component';
-import { HomeComponent } from 'src/app/pages/home/home.component';
 
 describe('[Unit] - AdoteComponent', (): void => {
     let component: AdoteComponent;
